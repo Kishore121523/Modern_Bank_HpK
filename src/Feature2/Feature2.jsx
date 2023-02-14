1cc4df6871967b1a64e966cca574e32968e4d47d
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2023-02-14 20:01:01 +0530

    Feature 2 completed

--- src/Feature2/Feature2.jsx
+++ src/Feature2/Feature2.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 
+import {bill,apple,google} from "../Assets"
+
 import styles from './Feature2.module.css';
 
 const Feature2 = () => {
   return (
     <div className={styles.outerDiv}>
-      <div className={styles.text}>Feature2</div>
+      <div className={styles.whiteGradient2}></div>
+      <div className={styles.pinkGradient}></div>
+      <img src={bill} alt="billImg" className={styles.billImg} />
+      <div className={styles.rightBillDiv}>
+        <p className={styles.rightBillHead}>
+          Easily control your billing & invoicing.
+        </p>
+        <div className={styles.rightBillMiddle}>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </div>
+        <div className={styles.rightBillBottom}>
+          <img src={apple} alt="apple" />
+          <img src={google} alt="google" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Feature2
